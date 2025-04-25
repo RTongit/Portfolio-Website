b081050408a01d6eb036a4b46b25f259e31b0294
meme wizard <rohantamuli27@gmail.com>
2025-04-25 14:25:59 +0530

    new commt

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,13 +1,8 @@
 import "/src/App.css";
 export default function Projects() {
-    const navStyle={
-     display : "flex",
-     flexDirection : "column",
-    
-    }
 
     return(
-        <nav style={navStyle}>
+        <nav className="navStyle">
             <h2>List of Project Names : </h2>
         <a href="https://github.com/RTongit/WeatherApp.git" className="projectLink" target="_blank">Weather App</a>
         <a href="https://github.com/RTongit/MusicPlayer.git" className="projectLink" target="_blank">Music Player</a>
